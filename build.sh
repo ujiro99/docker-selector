36458b2236bc57fadbfbc34752763c4d6ce116e6
ujiro99 <siro.cola@gmail.com>
2018-01-03 14:50:55 +0900

    [add] Add build flags for binary size down.

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 bin_name=docker-selector
 
-GOOS=linux   GOARCH=amd64 go build -o ./dist/linux_64_${bin_name}
-GOOS=linux   GOARCH=386   go build -o ./dist/linux_386_${bin_name}
-GOOS=windows GOARCH=386   go build -o ./dist/windows_386_${bin_name}.exe
-GOOS=windows GOARCH=amd64 go build -o ./dist/windows_64_${bin_name}.exe
-GOOS=darwin  GOARCH=386   go build -o ./dist/darwin_386_${bin_name}
-GOOS=darwin  GOARCH=amd64 go build -o ./dist/darwin_64_${bin_name}
+GOOS=linux   GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/linux_64_${bin_name}
+GOOS=linux   GOARCH=386   go build -ldflags="-s -w" -o ./dist/linux_386_${bin_name}
+GOOS=windows GOARCH=386   go build -ldflags="-s -w" -o ./dist/windows_386_${bin_name}.exe
+GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/windows_64_${bin_name}.exe
+GOOS=darwin  GOARCH=386   go build -ldflags="-s -w" -o ./dist/darwin_386_${bin_name}
+GOOS=darwin  GOARCH=amd64 go build -ldflags="-s -w" -o ./dist/darwin_64_${bin_name}
 
 cd dist
 
