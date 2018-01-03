381d9cda948dda38ea1c3361d0f19f8bbff0ed32
ujiro99 <siro.cola@gmail.com>
2018-01-03 13:05:57 +0900

    [fix] Fix build script.
    - Cross compile manually instead of using gox.

--- build.sh
+++ build.sh
@@ -1,16 +1,27 @@
-VERSION=$(git describe --tags)
-gox -output "dist/{{.OS}}_{{.Arch}}_{{.Dir}}" -os="windows darwin linux" -ldflags="-s -w -X main.version=${VERSION}"
+#!/bin/sh
+
+bin_name=docker-selector
+
+GOOS=linux   GOARCH=amd64 go build -o ./dist/linux_64_${bin_name}
+GOOS=linux   GOARCH=386   go build -o ./dist/linux_386_${bin_name}
+GOOS=windows GOARCH=386   go build -o ./dist/windows_386_${bin_name}.exe
+GOOS=windows GOARCH=amd64 go build -o ./dist/windows_64_${bin_name}.exe
+GOOS=darwin  GOARCH=386   go build -o ./dist/darwin_386_${bin_name}
+GOOS=darwin  GOARCH=amd64 go build -o ./dist/darwin_64_${bin_name}
+
 cd dist
-mv  darwin_386_docker-selector        docker-selector
-zip darwin_386_docker-selector        docker-selector     -qm
-mv  darwin_amd64_docker-selector      docker-selector
-zip darwin_amd64_docker-selector      docker-selector     -qm
-mv  linux_386_docker-selector         docker-selector
-zip linux_386_docker-selector         docker-selector     -qm
-mv  linux_amd64_docker-selector       docker-selector
-zip linux_amd64_docker-selector       docker-selector     -qm
-mv  windows_386_docker-selector.exe   docker-selector.exe
-zip windows_386_docker-selector       docker-selector.exe -qm
-mv  windows_amd64_docker-selector.exe docker-selector.exe
-zip windows_amd64_docker-selector     docker-selector.exe -qm
+
+mv  linux_386_${bin_name}       ${bin_name}
+zip linux_386_${bin_name}       ${bin_name}     -qm
+mv  linux_64_${bin_name}        ${bin_name}
+zip linux_64_${bin_name}        ${bin_name}     -qm
+mv  windows_386_${bin_name}.exe ${bin_name}.exe
+zip windows_386_${bin_name}     ${bin_name}.exe -qm
+mv  windows_64_${bin_name}.exe  ${bin_name}.exe
+zip windows_64_${bin_name}      ${bin_name}.exe -qm
+mv  darwin_386_${bin_name}      ${bin_name}
+zip darwin_386_${bin_name}      ${bin_name}     -qm
+mv  darwin_64_${bin_name}       ${bin_name}
+zip darwin_64_${bin_name}       ${bin_name}     -qm
+
 cd -
